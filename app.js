4db6f8ea7f419e4af250615ebe6788cb40601e44
arews <are.w.sandvik@gmail.com>
2019-12-10 15:47:28 +0100

    merging development into master

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-    
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
 const passport = require("passport");
 
 require("./configs/passport");
 
-
 mongoose
-  .connect('mongodb://localhost/gerrit-project3', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/gerrit-project3", {
+    useNewUrlParser: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-    
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // session settings
 app.use(
@@ -69,7 +75,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-  
 // not necessary
 // hbs.registerHelper('ifUndefined', (value, options) => {
 //   if (arguments.length < 2)
@@ -80,10 +85,9 @@ app.use(passport.session());
 //       return options.fn(this);
 //   }
 // });
-  
 
 // default value for title local
-app.locals.title = 'Gerrit - Project3 ';
+app.locals.title = "Gerrit - Project3 ";
 
 // // Enable authentication using session + passport
 // app.use(session({
@@ -94,18 +98,23 @@ app.locals.title = 'Gerrit - Project3 ';
 // }))
 // app.use(flash());
 // require('./passport')(app);
-    
-// routes 
-const index = require('./routes/index');
-app.use('/', index);
 
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
+// routes
+const index = require("./routes/index");
+app.use("/", index);
 
-const chatRoutes = require('./routes/chat');
-app.use('/chat', chatRoutes);
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
-const profileRoutes = require('./routes/profile');
-app.use('/profile', profileRoutes);
+const chatRoutes = require("./routes/chat");
+app.use("/chat", chatRoutes);
+
+const profileRoutes = require("./routes/profile");
+app.use("/profile", profileRoutes);
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;

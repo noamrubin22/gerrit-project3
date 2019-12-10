0d1c451bb18207ab35d75990cd552742ebf532cf
Noam <noamru22@gmail.com>
2019-12-10 17:38:34 +0100

    deploy with react

---
+++ client/build/precache-manifest.81ba68511c57c57580a1a5b82e8e3f28.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194d1ddf8f18c1b32f8dfbd9286e5e29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596a94b9cfba2c83797f",
+    "url": "/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "9f2040fceeb931d391d1",
+    "url": "/static/css/main.b83583c0.chunk.css"
+  },
+  {
+    "revision": "596a94b9cfba2c83797f",
+    "url": "/static/js/2.7caa93f1.chunk.js"
+  },
+  {
+    "revision": "9f2040fceeb931d391d1",
+    "url": "/static/js/main.69a1465e.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "23b85bf98f5082d6698f8e6593c2eb48",
+    "url": "/static/media/green3.23b85bf9.png"
+  },
+  {
+    "revision": "188eef9cce6368e2f3fd19b6528a7575",
+    "url": "/static/media/grey.188eef9c.png"
+  },
+  {
+    "revision": "fcb95cb19496356eaaf482fc9b825670",
+    "url": "/static/media/hermannplatz_round.fcb95cb1.png"
+  },
+  {
+    "revision": "ca7de6b95f69d9ea6f06c33e23e930ee",
+    "url": "/static/media/red2.ca7de6b9.png"
+  }
+]);

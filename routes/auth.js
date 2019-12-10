5932b8f8531657b35a82468d9cc2d444da95fcec
Noam <noamru22@gmail.com>
2019-12-10 12:36:58 +0100

    default image added

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-
 router.post("/signup", (req, res) => {
-  const {
-    username,
-    password
-  } = req.body;
+  const { username, password } = req.body;
 
   if (!username) {
     return res.status(400).json({
@@ -23,8 +19,8 @@ router.post("/signup", (req, res) => {
   }
 
   User.findOne({
-      username: username
-    })
+    username: username
+  })
     .then(found => {
       if (found) {
         return res.status(400).json({
@@ -39,7 +35,9 @@ router.post("/signup", (req, res) => {
         .then(hash => {
           return User.create({
             username: username,
-            password: hash
+            password: hash,
+            image:
+              "https://res.cloudinary.com/justgerrit/image/upload/v1575891083/profilepictures/gerrit_xiixvp.jpg"
           });
         })
         .then(newUser => {
@@ -63,33 +61,33 @@ router.post("/login", (req, res, next) => {
     if (err) {
       return res.status(500).json({
         message: "Error while authenticating"
-      })
+      });
     }
     if (!user) {
       // no user found with username or password didn't match
       return res.status(400).json({
         message: "Invalid credentials"
-      })
+      });
     }
-    // passport req.login 
+    // passport req.login
     // creates the passport session
-    req.login(user, (err) => {
-      if (err) res.status(500).json(err)
+    req.login(user, err => {
+      if (err) res.status(500).json(err);
       res.json(user);
-    })
+    });
   })(req, res, next);
-})
+});
 
 router.delete("/logout", (req, res) => {
   // passport logout function
   req.logout();
   res.json({
     message: "Logout was succesful"
-  })
-})
+  });
+});
 
 router.get("/loggedin", (req, res) => {
   res.json(req.user);
-})
+});
 
 module.exports = router;

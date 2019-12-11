55629387f78d2135ea099b1b1b2416f9a5bffb27
Noam <noamru22@gmail.com>
2019-12-11 21:10:14 +0100

    users can click on other users and get into profile

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,46 +1,49 @@
 import React, { useState, useEffect } from "react";
 
 const Message = props => {
-  let {username, content, created_at, userId} = props.message;
-  created_at = created_at.slice(11,16);
+  let { username, content, created_at, userId } = props.message;
+  created_at = created_at.slice(11, 16);
 
   const generalStyle = {
     backgroundColor: "white",
     margin: "10px 0 10px 0",
     width: "80vw"
-  }
+  };
 
   const ownStyle = {
     backgroundColor: "yellow",
     margin: "10px 0 10px 0",
     width: "80vw",
     alignSelf: "flex-end"
-  }
-  
-  
-  return(
+  };
+
+  return (
     <>
-    {props.user._id === userId ? 
-      <div className="own-message">
-      <div className="own-message-body">
-        <p className="username">{username}</p>
-        <p className="message-content">{content}</p>
-        <p className="message-timestamp">{created_at}</p>
-      </div>
-      <div className="arrow-right"></div>
-    </div>
-    : 
-    <div className="other-message">
-      <div className="arrow-left"></div>
-      <div className="other-message-body">
-        <p className="username">{username}</p>
-        <p className="message-content">{content}</p>
-        <p className="message-timestamp">{created_at}</p>
-      </div>
-    </div>}
+      {props.user._id === userId ? (
+        <div className="own-message">
+          <div className="own-message-body">
+            <a href={`/profile/${props.user._id}`}>
+              <p className="username">{username}</p>
+            </a>
+            <p className="message-content">{content}</p>
+            <p className="message-timestamp">{created_at}</p>
+          </div>
+          <div className="arrow-right"></div>
+        </div>
+      ) : (
+        <div className="other-message">
+          <div className="arrow-left"></div>
+          <div className="other-message-body">
+            <a href={`/profile/${userId}`}>
+              <p className="username">{username}</p>
+            </a>
+            <p className="message-content">{content}</p>
+            <p className="message-timestamp">{created_at}</p>
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Message;
-
